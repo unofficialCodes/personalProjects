4c40e19ec1d63c5896cc5b7e3a3d28c785a04d1b
Programmer <programmer@abc.com>
2021-09-16 15:24:19 +0530

    fixed media queries v2

--- colorFlipper.js
+++ colorFlipper.js
@@ -1,65 +1,71 @@
-console.log('ColorFlipper App is running ..');
+console.log("ColorFlipper App is running ..");
 
 //Initiate arrays
-const colorArr = ['green','blue','red','lightseagreen','lightblue','#ffff00'];
+const colorArr = [
+  "green",
+  "blue",
+  "red",
+  "lightseagreen",
+  "lightblue",
+  "#ffff00",
+];
 
 //get span element
-const span = document.getElementById('colorText');
+const span = document.getElementById("colorText");
 
 let count = 0;
 
 //Simple Colors button
-let colorBtn = document.getElementById('color');
-colorBtn.addEventListener('click', colorHandler);
+let colorBtn = document.getElementById("color");
+colorBtn.addEventListener("click", colorHandler);
 
 //Hex Colors button
-let hex = document.getElementById('hexBtn');
-hex.addEventListener('click',hexHandler);
+let hex = document.getElementById("hexBtn");
+hex.addEventListener("click", hexHandler);
 
 //default backgroundColor
-document.body.style.backgroundColor = '#eeebf0';
+document.body.style.backgroundColor = "#eeebf0";
 
 //simple colors click handler function
-function colorHandler(){
-	for(let i = 0;i<colorArr.length;i++) {
-		count = Math.floor(Math.random() * (colorArr.length));
-	}
-	document.body.style.backgroundColor = colorArr[count];
-	span.textContent = colorArr[count];
+function colorHandler() {
+  for (let i = 0; i < colorArr.length; i++) {
+    count = Math.floor(Math.random() * colorArr.length);
+  }
+  document.body.style.backgroundColor = colorArr[count];
+  span.textContent = colorArr[count];
 }
 
 //Hex Colors click handler function
-const colors = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+const colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-let str = '#';
+let str = "#";
 function hexHandler() {
-	let newBtn = document.createElement('button');
-	colorBtn.insertAdjacentElement('afterend',newBtn);
+  let newBtn = document.createElement("button");
+  colorBtn.insertAdjacentElement("afterend", newBtn);
 
-	newBtn.textContent = 'Hex Colors';
-	newBtn.classList.add('surprise');
-	newBtn.id = 'supriseBtn';
-	newBtn.addEventListener('click', generateRandom);
-	// generateRandom();
+  newBtn.textContent = "Hex Colors";
+  newBtn.classList.add("surprise");
+  newBtn.id = "supriseBtn";
+  newBtn.addEventListener("click", generateRandom);
+  // generateRandom();
 }
-function getRandom(){
-	return Math.floor(Math.random() * colors.length - 1);
+function getRandom() {
+  return Math.floor(Math.random() * colors.length - 1);
 }
-
 
 function generateRandom() {
-	for(let k = 0;k<6;k++){
-		str += colors[getRandom()];
-		if(str.length<7){
-			str += colors[getRandom()];
-		}
-		if(str.length>7){
-			str = '#';
-		}else if(str.length == 7){
-			document.body.style.backgroundColor = str;
-			span.textContent = str;
-		}
-	}
+  for (let k = 0; k < 6; k++) {
+    str += colors[getRandom()];
+    if (str.length < 7) {
+      str += colors[getRandom()];
+    }
+    if (str.length > 7) {
+      str = "#";
+    } else if (str.length == 7) {
+      document.body.style.backgroundColor = str;
+      span.textContent = str;
+    }
+  }
 }
 
 //if else statements with break and continue;
@@ -73,12 +79,19 @@ function generateRandom() {
 // 			console.log(str);
 // 		}
 
-let surpriseBtn = document.getElementById('surprise');
+let surpriseBtn = document.getElementById("surprise");
 
-surpriseBtn.addEventListener('click', surpriseHandler);
+surpriseBtn.addEventListener("click", surpriseHandler);
 
 function surpriseHandler() {
-	console.log('Will be available soon ..');
+  let promptValue = confirm(
+    'New Feature will be avaiable soon ...To Try the Beta Version click "OK"(This will change the Text Shadows)'
+  );
+  if (promptValue == true) {
+    let navigation = document.getElementById("navigation");
+    navigation.style.textShadow = "2px 2px 6px #000";
+  }
+  console.log("Will be available soon ..");
 }
 
 // function button() {
